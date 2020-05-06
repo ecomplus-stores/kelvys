9af708faff68adb308ced0f7ed0ffe4712b732df
KelvysCamisas <52542731+KelvysCamisas@users.noreply.github.com>
2020-05-06 15:26:00 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,7 @@
 // Add your custom JavaScript for storefront pages here.
+// Adicionar Formas de Envio
+$( "footer.footer.py-3.py-lg-4.py-xl-5 > .container" ).append("<div class='row'><div class='div.col-12.col-sm-6.col-lg'>"
++ "<big>Formas de envio</big>"
++ "<img src='https://cdn.e-com.club/s/www.kelvys.com.br/uploads/kelvys-logo-correios.png' alt='Correios' title='PAC e SEDEX'>"
++ "<img src='https://cdn.ecvol.com/s/www.kelvys.com.br/uploads/logo-asaplog-kelvys.png' alt='AsapLog' title='TRANSPORTADORA' style='margin-top: 20px;'></div></div>");
+//Fim
