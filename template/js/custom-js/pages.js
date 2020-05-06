bccdc47281caf10d15883c0825bdb187e3f71061
KelvysCamisas <52542731+KelvysCamisas@users.noreply.github.com>
2020-05-06 15:44:28 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -5,3 +5,5 @@ $( "footer.footer.py-3.py-lg-4.py-xl-5 > .container" ).append("<div class='row'>
 + "<img src='https://cdn.e-com.club/s/www.kelvys.com.br/uploads/kelvys-logo-correios.png' alt='Correios' title='PAC e SEDEX'>"
 + "<img src='https://cdn.ecvol.com/s/www.kelvys.com.br/uploads/logo-asaplog-kelvys.png' alt='AsapLog' title='TRANSPORTADORA' style='margin-top: 20px;'></div></div>");
 //Fim
+$('<div class="rastrear-pedido"></div>').appendTo('footer.footer.py-3.py-lg-4.py-xl-5');
+     $('<a href="http://www2.correios.com.br/sistemas/rastreamento/" target="_blank" style="border-right: 1px solid #ab0000; padding-right: 20px;"  "<b>Rastrear pedido</b></a>').appendTo('.rastrear-pedido');
