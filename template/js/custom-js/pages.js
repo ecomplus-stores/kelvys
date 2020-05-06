26615119b4b1a36ff7c822a650d8ad0c0db5d6b3
KelvysCamisas <52542731+KelvysCamisas@users.noreply.github.com>
2020-05-06 17:52:34 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -3,5 +3,5 @@
 $( "div.col-12.col-lg-auto.order-lg-last" ).append("<div class='row'><div class='forma-de-envio'>"
 + "<big>Formas de envio</big>"
 + "<img src='https://cdn.e-com.club/s/www.kelvys.com.br/uploads/kelvys-logo-correios.png' alt='Correios' title='PAC e SEDEX'>"
- + "<img src='https://cdn.ecvol.com/s/www.kelvys.com.br/uploads/logo-jadlog1-kelvys.png' alt='JadLog' title='.Com'>"                                                  
++ "<img src='https://cdn.ecvol.com/s/www.kelvys.com.br/uploads/logo-jadlog1-kelvys.png' alt='JadLog' title='.Com'>"                                                  
 + "<img src='https://cdn.ecvol.com/s/www.kelvys.com.br/uploads/logo-asaplog-kelvys.png' alt='AsapLog' title='TRANSPORTADORA'></div></div>");
