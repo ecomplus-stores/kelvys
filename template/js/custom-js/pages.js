437522082008cc051defbba618e7916e9d1754da
KelvysCamisas <52542731+KelvysCamisas@users.noreply.github.com>
2020-05-06 19:41:44 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,13 +1,1 @@
 // Add your custom JavaScript for storefront pages here.
-// Adicionar Formas de Envio
-$( "div.col-12.col-lg-auto.order-lg-last" ).append("<div class='row'><div class='forma-de-envio'>"
-+ "<big>Formas de envio</big>"
-+ "<img src='https://cdn.e-com.club/s/www.kelvys.com.br/uploads/kelvys-logo-correios.png' alt='Correios' title='PAC e SEDEX'>"
-+ "<img src='https://cdn.ecvol.com/s/www.kelvys.com.br/uploads/logo-jadlog1-kelvys.png' alt='JadLog' title='.Com'>"                                                  
-+ "<img src='https://cdn.ecvol.com/s/www.kelvys.com.br/uploads/logo-asaplog-kelvys.png' alt='AsapLog' title='TRANSPORTADORA'></div></div>");
-
-$( "div.col-12.col-xl-5.order-lg-first" ).append("<div class='row'><div class='forma-de-envio'>"
-+ "<big>Formas de envio</big>"
-+ "<img src='https://cdn.e-com.club/s/www.kelvys.com.br/uploads/kelvys-logo-correios.png' alt='Correios' title='PAC e SEDEX'>"
-+ "<img src='https://cdn.ecvol.com/s/www.kelvys.com.br/uploads/logo-jadlog1-kelvys.png' alt='JadLog' title='.Com'>"                                                  
-+ "<img src='https://cdn.ecvol.com/s/www.kelvys.com.br/uploads/Banner-Tarja-350-min.png' alt='AsapLog' title='TRANSPORTADORA'></div></div>");
