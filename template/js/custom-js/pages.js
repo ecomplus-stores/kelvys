db94c414a674484e1275b0ea2fb4781c986c4ff9
KelvysCamisas <52542731+KelvysCamisas@users.noreply.github.com>
2020-05-06 16:05:16 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,9 +1,10 @@
 // Add your custom JavaScript for storefront pages here.
 // Adicionar Formas de Envio
-$( "footer.footer.py-3.py-lg-4.py-xl-5 > .container" ).appendTo("<div class='row'><div class='div.col-12.col-sm-6.col-lg'>"
+$( "footer.footer.py-3.py-lg-4.py-xl-5 > .container" ).append("<div class='row'><div class='div.col-12.col-lg-auto.order-lg-last'>"
 + "<big>Formas de envio</big>"
 + "<img src='https://cdn.e-com.club/s/www.kelvys.com.br/uploads/kelvys-logo-correios.png' alt='Correios' title='PAC e SEDEX'>"
 + "<img src='https://cdn.ecvol.com/s/www.kelvys.com.br/uploads/logo-asaplog-kelvys.png' alt='AsapLog' title='TRANSPORTADORA' style='margin-top: 20px;'></div></div>");
 //Fim
-$('<div class="rastrear-pedido"></div>').appendTo('div.col-12.col-xl-5.order-lg-first');
+$('<div class="rastrear-pedido"></div>').appendTo('div.col-12.col-lg-auto.order-lg-last');
      $('<a href="http://www2.correios.com.br/sistemas/rastreamento/" target="_blank" style="border-right: 1px solid #ab0000; padding-right: 20px;"  "<b>Rastrear pedido</b></a>').appendTo('.rastrear-pedido');
++ "<img src='https://cdn.e-com.club/s/www.kelvys.com.br/uploads/kelvys-logo-correios.png' alt='Correios' title='PAC e SEDEX'>"
