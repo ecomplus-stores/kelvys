5743304cbbfbaca6f2f8f6e403365fbac3d7f6cf
KelvysCamisas <52542731+KelvysCamisas@users.noreply.github.com>
2020-05-06 16:49:28 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,6 +1,6 @@
 // Add your custom JavaScript for storefront pages here.
 // Adicionar Formas de Envio
-$('<div class="selos-safety"></div>').append("<div class='row'><div class='div.col-12.col-sm-6.col-lg'>"
+$('<div class="selos-safety"></div>').appendTo('div.col-12.col-sm-6.col-lg'>"
 $('<img src="https://cdn.ecvol.com/s/www.kelvys.com.br/uploads/logo-asaplog-kelvys.png" alt="ASAP Log" title="Transportadora"><big>Certificados</big>').appendTo('.selos-safety');
 //Fim
 $('<div class="forma-de-envio"></div>').appendTo('div.col-12.col-lg-auto.order-lg-last');
