91013649fa265067f94ed237228e97c00ddeaa7e
KelvysCamisas <52542731+KelvysCamisas@users.noreply.github.com>
2020-05-06 20:23:58 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,6 +1,2 @@
 // Add your custom JavaScript for storefront pages here.
-// Adicionar Formas de Envio
-$( "div.col-12.col-lg-auto.order-lg-last" ).append("<div class='row'><div class='forma-de-envio'>"
-+ "<big>Formas de envio</big>"
-+ "<img src='https://cdn.ecvol.com/s/www.kelvys.com.br/uploads/banner-envios-web.jpg' title='Formas de envio'></div></div>");
-//Fim
+
