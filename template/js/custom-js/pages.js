4e93124b02d885276af64ff37772b5fe29183981
KelvysCamisas <52542731+KelvysCamisas@users.noreply.github.com>
2020-05-11 12:07:52 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,2 +1,2 @@
 // Add your custom JavaScript for storefront pages here.
-$( "div.sections.pb-1.pb-sm-2.pb-lg-4" ).append("<div class='row'><div class='div.search-engine__nav'>"
+$( "div.col-12.col-lg-auto.order-lg-last" ).append("<div class='row'><div class='div.search-engine__nav'>"
