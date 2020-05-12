f23ff7c8e5fbd7fbd881a590d1aa2d821abe180c
KelvysCamisas <52542731+KelvysCamisas@users.noreply.github.com>
2020-05-12 13:30:18 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,7 @@
 // Add your custom JavaScript for storefront pages here.
+//Chat WhatsApp
+$('body#page-categories, body#page-products, body#page-search').append('<a href="https://web.whatsapp.com/send?1=pt_BR&phone=554491808130" target="_blank" id="ws_icon_chat" title="Atendimento via WhatsApp"><img src="https://cdn.e-com.club/s/www.kelvys.com.br/uploads/whatsapp_kelvys.png"></a>');
+if($(window).width() < 480) {
+$("#ws_icon_chat").attr("href", "https://api.whatsapp.com/send?1=pt_BR&phone=554491808130");
+}
+//Fim
