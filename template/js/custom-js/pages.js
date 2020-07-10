45293136cd81472840ba6b2feaec712b25b3f107
KelvysCamisas <52542731+KelvysCamisas@users.noreply.github.com>
2020-07-10 17:51:49 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -5,9 +5,3 @@ if($(window).width() < 480) {
 $("#ws_icon_chat").attr("href", "https://api.whatsapp.com/send?1=pt_BR&phone=554491808130");
 }
 //Fim
-
-{
-  <meta class="netreviewsWidget" id="netreviewsWidgetNum3835" data-jsurl="//cl.avis-verifies.com/br/cache/8/c/7/8c7c2c69-7520-d754-f544-483d6a0625cb/widget4/widget01-3835_script.js"/><script src="//cl.avis-verifies.com/br/widget4/widget01_FB3.min.js"></script>
-  },
-  "stampsAppend": "src/append/stamps"
-}
