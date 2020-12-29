bbe21d762ec9e6691f0e308e00d4a6edb0d8b592
KelvysCamisas <52542731+KelvysCamisas@users.noreply.github.com>
2020-12-29 17:57:37 -0300

    Update checkout.js

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -1,4 +1,5 @@
 import '#template/js/checkout'
 import './custom-js/checkout'
 
-window.ecomPaymentApps = [1250, 111223, 1251]
+window.ecomPaymentApps = [1250, 111223, 101827, 1251]
+window.ecomShippingApps = [101827, 1251, 111223]
