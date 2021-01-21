9222e64e69d3bc6fbb52498acf12f58ac6337fa9
KelvysCamisas <52542731+KelvysCamisas@users.noreply.github.com>
2021-01-21 13:03:32 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -5,3 +5,11 @@ if($(window).width() < 480) {
 $("#ws_icon_chat").attr("href", "https://api.whatsapp.com/send?1=pt_BR&phone=554491808130");
 }
 //Fim
+// Cria campo novo de certificados e move eles pra dentro
+if(screen.width >= 768){ 
+   // cria campo novo embaixo do footer
+  $('<div class="certificados"></div>').appendTo('.footer.py-3.py-lg-4.py-xl-5');
+  // move os selos pra dentro desse novo campo
+  $('.footer__stamps').appendTo('.certificados'); 
+}
+//Fim
