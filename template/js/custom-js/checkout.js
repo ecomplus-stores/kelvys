02848e79c72b379664b585f02b705a2206d2c9e8
KelvysCamisas <52542731+KelvysCamisas@users.noreply.github.com>
2021-10-19 12:07:07 -0300

    Update checkout.js

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -1,12 +1,1 @@
 // Add your custom JavaScript for checkout here.
-// Mudança de Carrinho pra Sacola
-function changeCartPageTitle(title) {
-    const router = window.storefrontApp && window.storefrontApp.router 
-    const currentRoute = router.currentRoute;
-    if(currentRoute.name == 'cart') {
-        document.title = title;
-    }
-};
-window.onload = () => {
-    changeCartPageTitle('Minha Sacola');
-}
