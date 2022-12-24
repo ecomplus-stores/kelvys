699dd841fcf5b9eeb99de8c1ae8d85f16fb2b9a0
KelvysCamisas <52542731+KelvysCamisas@users.noreply.github.com>
2022-12-24 18:48:33 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -20,5 +20,5 @@ if(screen.width >= 768){
 }
 //Fim
 // Frete MercadoLivre
-$('.shipping-calculator__services::after').append('<a href="https://web.whatsapp.com/send?1=pt_BR&phone=554491808130" target="_blank" id="ws_icon_chat" title="Atendimento via WhatsApp"><img src="https://www.kelvys.com.br/img/uploads/whatsapp_kelvys.png"></a>');
+$('.shipping-calculator__services::after').appendTo('<a href="https://web.whatsapp.com/send?1=pt_BR&phone=554491808130" target="_blank" id="ws_icon_chat" title="Atendimento via WhatsApp"><img src="https://www.kelvys.com.br/img/uploads/whatsapp_kelvys.png"></a>');
 //Fim
