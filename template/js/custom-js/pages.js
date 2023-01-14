6b835133d4f83f5a7aa227c10cb59a23f9722104
KelvysCamisas <52542731+KelvysCamisas@users.noreply.github.com>
2023-01-13 21:11:21 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -19,3 +19,8 @@ if(screen.width >= 768){
   $('p.footer__payment-methods').appendTo('.formas-de-pagamento'); 
 }
 //Fim
+//ML
+$('shipping-calculator__services::after').append('<a href="https://web.whatsapp.com/send?1=pt_BR&phone=554491808130" target="_blank" id="ws_icon_chat" title="Atendimento via WhatsApp"><img src="https://www.kelvys.com.br/img/uploads/whatsapp_kelvys.png"></a>');
+if($(window).width() < 480) {
+$("#ws_icon_chat").attr("href", "https://api.whatsapp.com/send?1=pt_BR&phone=554491808130");
+}
