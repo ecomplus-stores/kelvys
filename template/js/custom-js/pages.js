c590bab22a6d593d92e8dcb5fa0809c66788f313
KelvysCamisas <52542731+KelvysCamisas@users.noreply.github.com>
2023-01-14 09:37:25 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -19,3 +19,8 @@ if(screen.width >= 768){
   $('p.footer__payment-methods').appendTo('.formas-de-pagamento'); 
 }
 //Fim
+//Frete Mercado Livre
+if(screen.width >= 768){
+  $('<div class="envio-ml"></div>').appendTo('.shipping-calculator');
+  $('<b>Pague com</b>').appendTo('.envio-ml'); 
+}
